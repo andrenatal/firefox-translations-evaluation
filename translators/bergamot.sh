8fd214421c746244da44d58843670c778033ad30
Andre Natal <anatal@gmail.com>
2023-04-20 21:09:13 -0700

    Remove shortlist

--- translators/bergamot.sh
+++ translators/bergamot.sh
@@ -7,7 +7,6 @@ SRCVOCAB=$(find "${MODEL_DIR}" -name srcvocab.*.spm)
 TRGVOCAB=$(find "${MODEL_DIR}" -name trgvocab.*.spm)
 VOCAB=$(find "${MODEL_DIR}" -name vocab.*.spm)
 MODEL=$(find "${MODEL_DIR}" -name model.${SRC}${TRG}.*.bin)
-SHORTLIST=$(find "${MODEL_DIR}" -name *${SRC}${TRG}.s2t.bin)
 CONFIG="/tmp/bergamot.config.${SRC}${TRG}.yml"
 
 if [[ ${MODEL} == *.intgemm.alphas.bin ]]; then
@@ -23,7 +22,6 @@ cp translators/bergamot.config.yml "${CONFIG}"
 sed -i -e "s+MODEL+${MODEL}+g" "${CONFIG}"
 sed -i -e "s+SRCVOCAB+${SRCVOCAB:-$VOCAB}+g" "${CONFIG}"
 sed -i -e "s+TRGVOCAB+${TRGVOCAB:-$VOCAB}+g" "${CONFIG}"
-sed -i -e "s+SHORTLIST+${SHORTLIST}+g" "${CONFIG}"
 sed -i -e "s+PRECISION+${PRECISION}+g" "${CONFIG}"
 
 $APP_PATH --model-config-paths "${CONFIG}" --log-level=info
